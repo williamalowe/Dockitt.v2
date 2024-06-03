e080b9791ca116308ca9e6a33927bbe4cb57b382
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-03 14:24:52 +1000

    feat: add navbar-component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav id='navbar'>
-      
+      <div className='logoContainer'>
+        <div>D</div>
+        <div>ockett.</div>
+      </div>
+      <div className='linksContainer'>
+        <NavLink to={'/tasks'}>
+          Task List          
+          <div></div>
+        </NavLink>
+        <NavLink to={'/dashboard'}>
+          Dashboard
+          <div></div>
+        </NavLink>
+      </div>
     </nav>
   )
 }
