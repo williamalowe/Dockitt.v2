927750bc78455b85f72c81cb7f8598fa42035be1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-03 12:38:12 +1000

    func: add data-theme context

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <main className={styles.app}>
-      hello
+    <main className={styles.app} data-theme={darkTheme ? 'dark' : 'light'}>
+      
     </main>
   )
 }
