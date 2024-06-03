79c46787c9a9a012c0858be6ec29221da88c087f
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-03 14:43:47 +1000

    fix: resolve logo link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
@@ -7,10 +7,20 @@ import { motion } from 'framer-motion';
 const Navbar = ({ currentTheme, handleSwitch }) => {
   return (
     <nav id='navbar'>
-      <div className='logoContainer'>
-        <div>D</div>
-        <div>ockett.</div>
-      </div>
+      <motion.div 
+        className='logoContainer'
+        whileHover={{
+          scale: 1.1
+        }}
+        whileTap={{
+          scale: 0.9
+        }}
+        >
+        <Link to={'/'}>
+          <div>D</div>
+          <div>ockett.</div>
+        </Link>
+      </motion.div>
       <div className='linksContainer'>
         <NavLink to={'/tasks'}>
           Task List          
