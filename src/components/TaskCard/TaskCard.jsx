561d2ffe368abc13658574f601567ea39ef8b51b
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-04 15:31:03 +1000

    feat: add canceltask button

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -9,7 +9,8 @@ import { motion } from "framer-motion";
 import { useTaskContext } from "../../context/TasksContext/TaskContext";
 
 const TaskCard = ({ header, date, description, tag, priority, status }) => {
-  const { deleteTask, updateStatus, rollbackStatus } = useTaskContext();
+  const { deleteTask, updateStatus, rollbackStatus, cancelTask } =
+    useTaskContext();
   return (
     <div className={styles.card}>
       <div className={styles.tagContainer}>
@@ -26,6 +27,13 @@ const TaskCard = ({ header, date, description, tag, priority, status }) => {
         </p>
         {status !== "cancelled" ? (
           <div className={styles.buttons}>
+            <motion.button
+              onClick={() => cancelTask(date)}
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FontAwesomeIcon icon={faX} />
+            </motion.button>
             <motion.button
               onClick={() => rollbackStatus(date)}
               whileHover={{ scale: 1.2 }}
