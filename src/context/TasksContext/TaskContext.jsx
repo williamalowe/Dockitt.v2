39684fa81dd7876cf5bd95ef859edd993d8b9680
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-04 14:29:05 +1000

    func: add localStorage

--- src/context/TasksContext/TaskContext.jsx
+++ src/context/TasksContext/TaskContext.jsx
@@ -1,21 +1,27 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 // Step 1: Create a context
 const TaskContext = createContext();
 
 const TaskProvider = ({ children }) => {
   // Step 2: Set up state for tasks
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
 
   // Step 3: Define functions to manipulate tasks
   const addTask = (newTask) => setTasks([...tasks, newTask]);
   const deleteTask = (taskId) => setTasks(tasks.filter((task) => task.id !== taskId));
-  const toggleTaskCompletion = (taskId) =>
-    setTasks(tasks.map((task) => (task.id === taskId ? { ...task, completed: !task.completed } : task)));
+  // const toggleTaskCompletion = (taskId) =>
+  //   setTasks(tasks.map((task) => (task.id === taskId ? { ...task, completed: !task.completed } : task)));
 
   // Step 4: Provide the context and functions to the components
   return (
-    <TaskContext.Provider value={{ tasks, addTask, deleteTask, toggleTaskCompletion }}>
+    <TaskContext.Provider value={{ tasks, addTask, deleteTask }}>
       {children}
     </TaskContext.Provider>
   );
