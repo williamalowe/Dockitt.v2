3aebc01184156b9745364af0369661a4ca48abd1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-05 12:49:07 +1000

    styling: add taskstable max-height

--- src/components/TasksTable/TasksTable.jsx
+++ src/components/TasksTable/TasksTable.jsx
@@ -7,25 +7,18 @@ const TasksTable = () => {
 
   return (
     <div className={styles.table}>
-      <TableItem 
-        id='id'
-        task='task'
-        status='status'
-        priority='priority'
-      />
+      <TableItem id="id" task="task" status="status" priority="priority" />
       <div className={styles.tasks}>
-      {
-        tasks.map((task) => 
-        <TableItem 
-          key={task.date}
-          id={task.date}
-          task={task.title}
-          tag={task.tag}
-          status={task.status}
-          priority={task.priority}
-        />
-        )
-      }
+        {tasks.map((task) => (
+          <TableItem
+            key={task.date}
+            id={task.date}
+            task={task.title}
+            tag={task.tag}
+            status={task.status}
+            priority={task.priority}
+          />
+        ))}
       </div>
     </div>
   );
