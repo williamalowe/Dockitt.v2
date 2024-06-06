d568bad6b2401f0b2132f747d95d26b16dff67a5
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-06 12:31:50 +1000

    feat: add dashboard-component layout

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -3,7 +3,16 @@ import styles from './Dashboard.module.css';
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
-      
+      <div className={styles.left}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={styles.right}>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   )
 }
