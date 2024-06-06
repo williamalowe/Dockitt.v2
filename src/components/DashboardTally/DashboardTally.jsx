f978fb5cff31d70f49c73f330c5f0b90a89474e8
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-06 15:29:33 +1000

    func: add tallyitem props

--- src/components/DashboardTally/DashboardTally.jsx
+++ src/components/DashboardTally/DashboardTally.jsx
@@ -1,37 +1,55 @@
-import { useTaskContext } from '../../context/TasksContext/TaskContext';
-import TallyItem from '../TallyItem/TallyItem';
-import styles from './DashboardTally.module.css';
+import { useTaskContext } from "../../context/TasksContext/TaskContext";
+import TallyItem from "../TallyItem/TallyItem";
+import styles from "./DashboardTally.module.css";
 
 const DashboardTally = () => {
   const { tasks } = useTaskContext();
+  const totalTasks = tasks.filter((task) => task.status !== "cancelled").length;
+
   return (
     <div className={styles.tally}>
-      <TallyItem 
-        status='backlog'
-        number={1}
-        percentage='47'
-        color='var(--backlog)'
+      <TallyItem
+        status="backlog"
+        number={tasks.filter((task) => task.status === "backlog").length}
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "backlog").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--backlog)"
       />
-      <TallyItem 
-        status='in progress'
-        number='13'
-        percentage='47'
-        color='var(--in-progress'
+      <TallyItem
+        status="in progress"
+        number={tasks.filter((task) => task.status === "in progress").length}
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "in progress").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--in-progress"
       />
-      <TallyItem 
-        status='under review'
-        number='13'
-        percentage='47'
-        color='var(--under-review)'
+      <TallyItem
+        status="under review"
+        number={tasks.filter((task) => task.status === "under review").length}
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "under review").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--under-review)"
       />
-      <TallyItem 
-        status='completed'
-        number='13'
-        percentage='47'
-        color='var(--completed)'
+      <TallyItem
+        status="completed"
+        number={tasks.filter((task) => task.status === "completed").length}
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "completed").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--completed)"
       />
     </div>
-  )
-}
+  );
+};
 
-export default DashboardTally
+export default DashboardTally;
