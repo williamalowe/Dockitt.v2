ad473573b815a661f8cdf00ff4f0690f6937c035
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-06 18:52:33 +1000

    func: add barchartitem props

--- src/components/BarChartItem/BarChartItem.jsx
+++ src/components/BarChartItem/BarChartItem.jsx
@@ -1,18 +1,28 @@
-import { useRef, useEffect } from 'react';
-import styles from './BarChartItem.module.css';
+import { useRef, useEffect } from "react";
+import styles from "./BarChartItem.module.css";
+import { motion } from "framer-motion";
 
 const BarChartItem = ({ header, percentage, color }) => {
   const fillRef = useRef(null);
 
   useEffect(() => {
     fillRef.current.style.background = color;
-    fillRef.current.style.width = percentage + '%';
-  }, [])
+    fillRef.current.style.width = percentage + "%";
+  }, [percentage]);
   return (
-    <div className={styles.chartItem}>
-      <div className={styles.fill} ref={fillRef}><h5>{percentage}% - {header}</h5></div>
-    </div>
-  )
-}
+    <motion.div
+      className={styles.chartItem}
+      whileHover={{
+        scale: 1.1,
+      }}
+    >
+      <div className={styles.fill} ref={fillRef}>
+        <h5>
+          {percentage}% - {header}
+        </h5>
+      </div>
+    </motion.div>
+  );
+};
 
-export default BarChartItem
+export default BarChartItem;
--- src/components/DashboardBarChart/DashboardBarChart.jsx
+++ src/components/DashboardBarChart/DashboardBarChart.jsx
@@ -1,16 +1,51 @@
-import BarChartItem from '../BarChartItem/BarChartItem';
-import styles from './DashboardBarChart.module.css';
+import { useTaskContext } from "../../context/TasksContext/TaskContext";
+import BarChartItem from "../BarChartItem/BarChartItem";
+import styles from "./DashboardBarChart.module.css";
 
 const DashboardBarChart = () => {
+  const { tasks } = useTaskContext();
+  const totalTasks = tasks.filter((task) => task.status !== "cancelled").length;
+
   return (
     <div className={styles.barChart}>
-      <BarChartItem 
-        header='Backlog'
-        percentage={10}
-        color='var(--backlog)'
+      <BarChartItem
+        header="Backlog"
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "backlog").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--backlog)"
+      />
+      <BarChartItem
+        header="In Progress"
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "in progress").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--in-progress)"
+      />
+      <BarChartItem
+        header="Under Review"
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "under review").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--under-review)"
+      />
+      <BarChartItem
+        header="Completed"
+        percentage={Math.floor(
+          (tasks.filter((task) => task.status === "completed").length /
+            totalTasks) *
+            100
+        )}
+        color="var(--completed)"
       />
     </div>
-  )
-}
+  );
+};
 
-export default DashboardBarChart
+export default DashboardBarChart;
