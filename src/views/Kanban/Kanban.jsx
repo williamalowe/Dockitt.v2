42ec6f0c79ba421b9cc02608a9314c5f2be84696
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-08 17:29:33 +1000

    styles: add tasklist mobile support

--- src/views/Kanban/Kanban.jsx
+++ src/views/Kanban/Kanban.jsx
@@ -210,12 +210,36 @@ const Kanban = () => {
         </AnimatePresence>
       </div> */}
       <div className={styles.mobile}>
-      <TaskList
-                header={"Backlog"}
-                color={"var(--backlog)"}
-                tasksList={tasks.filter((task) => task.status === "backlog")}
-                count={tasks.filter((task) => task.status === "backlog").length}
-              />
+        <TaskList
+          header={"Backlog"}
+          color={"var(--backlog)"}
+          tasksList={tasks.filter((task) => task.status === "backlog")}
+          count={tasks.filter((task) => task.status === "backlog").length}
+        />
+        <TaskList
+          header={"In Progress"}
+          color={"var(--in-progress)"}
+          tasksList={tasks.filter((task) => task.status === "in progress")}
+          count={tasks.filter((task) => task.status === "in progress").length}
+        />
+        <TaskList
+          header={"Under Review"}
+          color={"var(--under-review)"}
+          tasksList={tasks.filter((task) => task.status === "under review")}
+          count={tasks.filter((task) => task.status === "under review").length}
+        />
+        <TaskList
+          header={"Completed"}
+          color={"var(--completed)"}
+          tasksList={tasks.filter((task) => task.status === "completed")}
+          count={tasks.filter((task) => task.status === "completed").length}
+        />
+        <TaskList
+          header={"Cancelled"}
+          color={"var(--cancelled)"}
+          tasksList={tasks.filter((task) => task.status === "cancelled")}
+          count={tasks.filter((task) => task.status === "cancelled").length}
+        />
       </div>
     </>
   );
