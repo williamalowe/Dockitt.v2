b782cd22e474cc36f80965dcfd1a6a87aaf2459d
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-08 15:54:07 +1000

    styles: add taskstable mobile support

--- src/components/TasksTable/TasksTable.jsx
+++ src/components/TasksTable/TasksTable.jsx
@@ -5,26 +5,27 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import TaskCard from "../TaskCard/TaskCard";
 
 const TasksTable = ({ taskslist }) => {
-
   return (
     <div className={styles.table}>
-      <TableItem id="id" task="task" status="status" priority="priority" />
+      <div className={styles.tableHeader}>
+        <TableItem id="id" task="task" status="status" priority="priority" />
+      </div>
+      <div className={styles.mobile}>Tasks List</div>
       <div className={styles.tasks}>
-        {
-        taskslist.length > 0 ?
-        taskslist.map((task) => (
-          <TableItem
-            key={task.date}
-            id={task.date}
-            task={task.title}
-            tag={task.tag}
-            status={task.status}
-            priority={task.priority}
-          />
-        ))
-        :
-        <div className={styles.empty}> No Items in this list!</div>
-      }
+        {taskslist.length > 0 ? (
+          taskslist.map((task) => (
+            <TableItem
+              key={task.date}
+              id={task.date}
+              task={task.title}
+              tag={task.tag}
+              status={task.status}
+              priority={task.priority}
+            />
+          ))
+        ) : (
+          <div className={styles.empty}> No Items in this list!</div>
+        )}
       </div>
       <div className={styles.arrow}>
         <FontAwesomeIcon icon={faCaretDown} />
